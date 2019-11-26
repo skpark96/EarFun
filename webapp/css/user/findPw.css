32a1c3b96bd47099f9ffd089e11ae3f32891be9b
soody <53030084+skpark96@users.noreply.github.com>
2019-11-27 00:07:16 +0900

    Add files via upload

---
+++ webapp/css/user/findPw.css
@@ -0,0 +1,103 @@
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+h1 {
+   text-align: center;
+}
+
+.find-page {
+   width: 500px;
+   padding: 8% 0 0;
+   margin: auto;
+}
+
+.form {
+   position: relative;
+   z-index: 1;
+   margin: 0 auto 100px;
+   padding: 45px;
+   /* text-align: center; */
+}
+
+.regId {
+   font-family: "Roboto", sans-serif;
+   outline: 0;
+   background: #f2f2f2;
+   width: 100%;
+   border: 0;
+   margin: 30px 0 15px;
+   padding: 15px;
+   box-sizing: border-box;
+   font-size: 17px;
+}
+
+.regEmail {
+   font-family: "Roboto", sans-serif;
+   outline: 0;
+   background: #f2f2f2;
+   width: 100%;
+   border: 0;
+   margin: 0 0 15px;
+   padding: 15px;
+   box-sizing: border-box;
+   font-size: 17px;
+}
+
+.subBtn {
+   font-family: "Roboto", sans-serif;
+   text-transform: uppercase;
+   outline: 0;
+   background: #2E6CC9;
+   width: 49%;
+   border: 0;
+   padding:15px;
+   color: #FFFFFF;
+   font-size: 17px;
+   -webkit-transition: all 0.3 ease;
+   transition: all 0.3 ease;
+   cursor: pointer;
+   margin-right: 10px;
+   margin-bottom: 10em;
+}
+
+.cancle {
+   font-family: "Roboto", sans-serif;
+   text-transform: uppercase;
+   outline: 0;
+   background: #2E6CC9;
+   width: 47%;
+   border: 0;
+   padding:15px;
+   color: #FFFFFF;
+   font-size: 17px;
+   -webkit-transition: all 0.3 ease;
+   transition: all 0.3 ease;
+   cursor: pointer;
+   margin-bottom: 10em;
+}
+
+.form button:hover, .form button:active, .form button:focus {
+   background: #214F94;
+}
+
+.form .message {
+   margin: 15px 0 0;
+   color: #b3b3b3;
+   font-size: 12px;
+}
+
+.form .message a {
+   color: #4CAF50;
+   text-decoration: none;
+}
+
+.form .register-form {
+   display: none;
+}
+
+.find {
+   text-align: center;
+}
+
+.a_find {
+   color: #877E7E;
+}
